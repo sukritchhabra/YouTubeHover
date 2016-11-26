a6ea03a46f7485610c1e4a72d126768fa962f5f4
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2016-11-25 21:12:15 -0500

    Creating delayOnHover module in options.
    * Setting up gulp to minify and concat module files.

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,17 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var cleanCSS = require('gulp-clean-css');
+
+var paths = {
+    components: 'components/*/*.js',
+    options_scripts: ['options/modules/*/*.js', '!options/modules/min/*.js'],
+    options_styles: ['options/modules/*/*.css', '!options/modules/min/*.css'],
+}
 
 // Task for Scripts (.js files in Compinents)
-gulp.task('scripts', function() {
-    return gulp.src('components/*/*.js')
+gulp.task('components', function() {
+    return gulp.src(paths.components)
       .pipe(concat('components.js'))
       .pipe(gulp.dest('components/'))
       .pipe(uglify())
@@ -14,11 +21,32 @@ gulp.task('scripts', function() {
       .pipe(gulp.dest('components/'));
 });
 
+gulp.task('optionsScripts', function() {
+    return gulp.src(paths.options_scripts)
+      .pipe(concat('modules.js'))
+      .pipe(gulp.dest('options/modules/'))
+      .pipe(uglify())
+      .pipe(rename("modules.min.js"))
+      .pipe(gulp.dest('options/modules/min/'));
+});
+
+gulp.task('optionsStyles', function() {
+    return gulp.src(paths.options_styles)
+      .pipe(concat('modules.css'))
+      .pipe(gulp.dest('options/modules/'))
+      .pipe(cleanCSS())
+      .pipe(rename("modules.min.css"))
+      .pipe(gulp.dest('options/modules/min/'));
+});
+
 // Watch task
 gulp.task('watch', function() {
     // Watch .js files
-    gulp.watch('components/*/*.js', ['scripts']);
+    gulp.watch('components/*/*.js', ['components']);
+    gulp.watch('options/modules/*/*.js', ['optionsScripts']);
+    gulp.watch('options/modules/*/*.css', ['optionsStyles']);
  });
 
 // Default Task
-gulp.task('default', ['watch', 'scripts']);
+gulp.task('default', ['watch', 'components', 'optionsScripts', 'optionsStyles']);
+
