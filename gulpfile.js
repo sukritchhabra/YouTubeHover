d7171fd013db0c34b89e43ea090ff7bc4d82f163
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2016-12-16 05:20:39 -0500

    Changing options.css -> options.scss. Also changing gulpfile.js.
    * Making options.css a SASS file.
    * Renaming gulp tasks to better relate to folders they work within.
    * Generating compiled (from scss) options.css file.

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,9 @@ var sass = require('gulp-sass');
 
 var paths = {
     controllers: 'controllers/*/*.js',
-    options_scripts: ['options/modules/*/*.js', '!options/modules/min/*.js'],
-    options_styles: ['options/modules/*/*.scss', '!options/modules/min/*.css'],
+    options_module_scripts: ['options/modules/*/*.js', '!options/modules/min/*.js'],
+    options_module_styles: ['options/modules/*/*.scss', '!options/modules/min/*.css'],
+    options_style: 'options/options.scss'
 }
 
 gulp.task('controllers', function() {
@@ -21,8 +22,8 @@ gulp.task('controllers', function() {
       .pipe(gulp.dest('controllers/'));
 });
 
-gulp.task('optionsScripts', function() {
-    return gulp.src(paths.options_scripts)
+gulp.task('optionsModuleScripts', function() {
+    return gulp.src(paths.options_module_scripts)
       .pipe(concat('modules.js'))
       .pipe(gulp.dest('options/modules/'))
       .pipe(uglify())
@@ -30,8 +31,8 @@ gulp.task('optionsScripts', function() {
       .pipe(gulp.dest('options/modules/min/'));
 });
 
-gulp.task('optionsStyles', function() {
-    return gulp.src(paths.options_styles)
+gulp.task('optionsModuleStyles', function() {
+    return gulp.src(paths.options_module_styles)
       .pipe(concat('modules.scss'))
       .pipe(gulp.dest('options/modules/'))
       .pipe(sass().on('error', sass.logError))
@@ -41,14 +42,21 @@ gulp.task('optionsStyles', function() {
       .pipe(gulp.dest('options/modules/min/'));
 });
 
+gulp.task('optionsStyle', function() {
+    return gulp.src(paths.options_style)
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('options/'))
+});
+
 // Watch task
 gulp.task('watch', function() {
     // Watch .js files
     gulp.watch('controllers/*/*.js', ['controllers']);
-    gulp.watch('options/modules/*/*.js', ['optionsScripts']);
-    gulp.watch('options/modules/*/*.scss', ['optionsStyles']);
+    gulp.watch('options/modules/*/*.js', ['optionsModuleScripts']);
+    gulp.watch('options/modules/*/*.scss', ['optionsModuleStyles']);
+    gulp.watch('options/options.scss', ['optionsStyle']);
  });
 
 // Default Task
-gulp.task('default', ['watch', 'controllers', 'optionsScripts', 'optionsStyles']);
+gulp.task('default', ['watch', 'controllers', 'optionsModuleScripts', 'optionsModuleStyles', 'optionsStyle']);
 
