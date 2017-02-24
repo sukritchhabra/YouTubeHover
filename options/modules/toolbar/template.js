8a160182d547f35063b1f06772828aff1a4d90be
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2017-02-24 12:26:35 -0500

    Refactoring toolbar module with IIFE's

--- options/modules/toolbar/template.js
+++ options/modules/toolbar/template.js
@@ -1,10 +1,35 @@
+var toolbarModule =
+(function ($) {
+    var collapseAll = {
+        init: function (argument) {
+            $('body').on('click', '#toolbar .collapse-all', function(event) {
+                collapseAll.collapse();
+            });
+        },
+
+        collapse: function () {
+            var $wrappers = $('section.option .wrapper:visible'); // Find non-hidden (displayed) wrappers
+            var $options = $wrappers.closest('section.option'); // Get the closest section.option parent of the hidden wrappers
+            $wrappers.hide('fast'); // Hide wrappers
+
+            var chevronGlyphs = $options.find('h4 span.fa'); // Get chevron glyphs corrosponding to the displayed wrappers.
+            $.each(chevronGlyphs, function (index, chevronGlyph) {
+                if ($(chevronGlyph).hasClass('fa-chevron-down')) {
+                    $(chevronGlyph).removeClass('fa-chevron-down').addClass('fa-chevron-up');
+                } else {
+                    $(chevronGlyph).removeClass('fa-chevron-up').addClass('fa-chevron-down');
+                }
+            })
+        }
+    };
+    collapseAll.init();
+})(window.jQuery);
+
 /**
  * Default toolbar function
  */
 function toolbar () {
-    $('body').on('click', '#toolbar .collapse-all', function(event) {
-        toolbar_collapseAll();
-    });
+    console.log('in toolbar');
 }
 
 /**
@@ -12,18 +37,3 @@ function toolbar () {
  * @param  {[JSON]} settings [Settings object]
  */
 function toolbar_restoreSettings (settings) {}
-
-function toolbar_collapseAll () {
-    var $wrappers = $('section.option .wrapper:visible'); // Find non-hidden (displayed) wrappers
-    var $options = $wrappers.closest('section.option'); // Get the closest section.option parent of the hidden wrappers
-    $wrappers.hide('fast'); // Hide wrappers
-
-    var chevronGlyphs = $options.find('h4 span.fa'); // Get chevron glyphs corrosponding to the displayed wrappers.
-    $.each(chevronGlyphs, function (index, chevronGlyph) {
-        if ($(chevronGlyph).hasClass('fa-chevron-down')) {
-            $(chevronGlyph).removeClass('fa-chevron-down').addClass('fa-chevron-up');
-        } else {
-            $(chevronGlyph).removeClass('fa-chevron-up').addClass('fa-chevron-down');
-        }
-    })
-}
