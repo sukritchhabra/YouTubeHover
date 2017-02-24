97d3d47e317876a3787d9e33f2622f274f146681
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2017-02-24 12:37:13 -0500

    Refactoring delayOnHover module with IIFE's

--- options/modules/delayOnHover/template.js
+++ options/modules/delayOnHover/template.js
@@ -1,26 +1,44 @@
+var delayOnHoverModule =
+(function ($) {
+    var hoverDelay = {
+        init: function (argument) {
+            // Initiate Slider
+            $('#delayOnHover .slider').slider({
+                min: 0,
+                max: 10,
+                step: 0.25,
+                animate: "fast",
+                change: function(event, slider) {
+                    hoverDelay.updateValue(slider.value, $(this).parent());
+                }
+            });
+        },
+
+        updateValue: function (delayVal, parentDiv) {
+            parentDiv.find('.saveValue').val(delayVal).trigger('change');
+        },
+
+        restore: function (settings) {
+            hoverDelay.init();
+            $('#delayOnHover .slider').slider('value', settings.delayOnHover.hoverDelay);
+            $('#delayOnHover .saveValue').val(settings.delayOnHover.hoverDelay);
+        }
+    };
+
+    return {
+        restoreHoverDelay: hoverDelay.restore
+    }
+
+})(window.jQuery);
+
 /**
  * Default function that binds default events for the delayOnHover module.
  * @return
  */
 function delayOnHover () {
-    // Initiate Slider
-    $('#delayOnHover .slider').slider({
-        min: 0,
-        max: 10,
-        step: 0.25,
-        animate: "fast",
-        change: function(event, slider) {
-            delayOnHover_updateDelayValue(slider.value, $(this).parent());
-        }
-    });
+    console.log('in delayOnHover');
 }
-
-function delayOnHover_updateDelayValue (delayVal, parentDiv) {
-        parentDiv.find('.saveValue').val(delayVal).trigger('change');
-}
-
 
 function delayOnHover_restoreSettings (settings) {
-    $('#delayOnHover .slider').slider('value', settings.delayOnHover.hoverDelay);
-    $('#delayOnHover .saveValue').val(settings.delayOnHover.hoverDelay);
+    delayOnHoverModule.restoreHoverDelay(settings);
 }
