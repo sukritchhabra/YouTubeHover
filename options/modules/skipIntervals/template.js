978b7d99b48e0248956214a8b38a830377de1ad9
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2017-02-24 12:21:05 -0500

    Refactoring skipIntervals module with IIFE's
    * Each setting for a module (.sub-option) has an object that stores the
    init, restore and other functions.
    
    * The individual restore functions are returned to the IIFE variable
    (global scope) to be used in the module restore function.

--- options/modules/skipIntervals/template.js
+++ options/modules/skipIntervals/template.js
@@ -1,75 +1,195 @@
+var skipIntervalsModule =
+(function ($) {
+    var enable = {
+        init: function (argument) {
+            $('body').on('change', '#skipIntervals #enable input[type="checkbox"]', function(event) {
+                var inputField = $(this);
+                var subOption = $(this).closest('.sub-option');
+
+                if (inputField.prop("checked")) {
+                    subOption.find('.saveValue').val("enabled");
+
+                    // Enabling settings change if module is enabled.
+                    subOption.siblings('.disableSettings').removeClass('disableSettings').addClass('enableSettings');
+                } else {
+                    subOption.find('.saveValue').val("disabled");
+
+                    // Disabling settings change if module is disabled.
+                    subOption.siblings('.enableSettings').removeClass('enableSettings').addClass('disableSettings');
+                }
+            });
+
+            // Prevent changes to settings if settings are disabled.
+            $('body').on('click', '.disableSettings', function(event) {
+                event.preventDefault();
+            });
+        },
+
+        restore: function (settings) {
+            enable.init();
+            var enabledSettingVal; // A variable to store the boolean value of the "enabled" setting
+            if (settings.skipIntervals.enabled == "enabled") {
+                enabledSettingVal = true;
+                $('#skipIntervals .addEnableSettingState').removeClass('addEnableSettingState').addClass('enableSettings');
+            } else {
+                enabledSettingVal = false;
+                $('#skipIntervals .addEnableSettingState').removeClass('addEnableSettingState').addClass('disableSettings');
+            }
+
+            // Load Enable checkbox value
+            $('#skipIntervals #enable input[type="checkbox"]').prop("checked", enabledSettingVal);
+            $('#skipIntervals #enable .saveValue').val(settings.skipIntervals.enabled);
+        }
+    };
+
+    var format = {
+        init: function (argument) {
+            $('body').on('change', '#skipIntervals #format input[name="format"]', function(event) {
+                var inputField = $(this);
+                var subOption = $(this).closest('.sub-option');
+                subOption.find('.saveValue').val(inputField.val());
+            });
+        },
+
+        restore: function (settings) {
+            format.init();
+            // Load Format radio button value
+            var formatRadioSelector = '#skipIntervals #format input[value="' + settings.skipIntervals.format + '"]';
+            $(formatRadioSelector).prop("checked", true);
+            $('#skipIntervals #format .saveValue').val(settings.skipIntervals.format);
+        }
+    };
+
+    var quality = {
+        init: function (argument) {
+            $('body').on('change', '#skipIntervals #quality input[name="quality"]', function(event) {
+                var inputField = $(this);
+                var subOption = $(this).closest('.sub-option');
+                subOption.find('.saveValue').val(inputField.val());
+            });
+        },
+
+        restore: function (settings) {
+            quality.init();
+            // Load Quality radio button value
+            var qualityRadioSelector = '#skipIntervals #quality input[value="' + settings.skipIntervals.quality + '"]';
+            $(qualityRadioSelector).prop("checked", true);
+            $('#skipIntervals #quality .saveValue').val(settings.skipIntervals.quality);
+        }
+    };
+
+    var increments = {
+        init: function (argument) {
+            $('body').on('change', '#skipIntervals #increments input[name="increments"]', function(event) {
+                var inputField = $(this);
+                var subOption = $(this).closest('.sub-option');
+                subOption.find('.saveValue').val(inputField.val());
+
+                // Emulating change event on slider by changing values. Using.trigger("change") was not working.
+                var tempSliderValue = $("#skipIntervals #increment-delay .slider").slider("option", "value");
+                $("#skipIntervals #increment-delay .slider").slider("option", "value", 0);
+                $("#skipIntervals #increment-delay .slider").slider("option", "value", tempSliderValue);
+            });
+        },
+
+        restore: function (settings) {
+            increments.init();
+            // Load Increments value
+            var incrementRadioSelector = '#skipIntervals #increments input[value="' + settings.skipIntervals.increment + '"]';
+            $(incrementRadioSelector).prop("checked", true);
+            $('#skipIntervals #increments .saveValue').val(settings.skipIntervals.increment);
+        }
+    };
+
+    var incrementDelay = {
+        init: function (argument) {
+            // Initiate Increment Delay Slider
+            $('#skipIntervals #increment-delay .slider').slider({
+                min: 0.5,
+                max: 10,
+                step: 0.5,
+                animate: "fast",
+                change: function(event, slider) {
+                    incrementDelay.updateValue(slider.value, $(this).parent());
+                }
+            });
+        },
+
+        /**
+         * Function that is called when a change occurs in the increment delay slider
+         * to update appropriate input fields.
+         * @param  {[Integer]} delayVal  [Value selected by the user in the slider]
+         * @param  {[DOM Element]} parentDiv [The parent div of the slider]
+         */
+        updateValue: function (delayVal, parentDiv) {
+            var incrementVal = parseInt($('#skipIntervals #increments .saveValue').val());
+            var totalDelay = delayVal * 1000;
+            var incrementFactor = Math.floor(totalDelay / incrementVal);
+
+            parentDiv.find('.increment-delay-value').val(delayVal);
+            parentDiv.find('.saveValue').val(incrementFactor).trigger('change');
+        },
+
+        restore: function (settings) {
+            incrementDelay.init();
+            // Load Increments Delay value
+            var sliderVal = (settings.skipIntervals.increment * settings.skipIntervals.incrementFactor) / 1000;
+            $("#skipIntervals #increment-delay .slider").slider("option", "value", sliderVal);
+            $('#skipIntervals #increment-delay .saveValue').val(settings.skipIntervals.incrementFactor);
+        }
+    };
+
+    var optimal = {
+        init: function (argument) {
+            $('#load-optimal-settings').on('click', function () {
+                var $button = $(this);
+                optimal.loadOptimalSettings("tiny", 20, 75);
+
+                $button.text('Loaded!');
+                $button.addClass('notify-load');
+                setTimeout(function() {
+                    $button.text('Load Optimal Settings');
+                    $button.removeClass('notify-load');
+                }, 1000);
+            });
+        },
+
+        /**
+         * Loads optimal setting if a user is facing trouble with choosing settings in accordance with their connection.
+         * @param  {[String]}  optimalQuality
+         * @param  {[Integer]} optimalIncrement
+         * @param  {[Integer]} optimalFactor
+         */
+        loadOptimalSettings: function (optimalQuality, optimalIncrement, optimalFactor) {
+            // Load Quality radio button value
+            var qualityRadioSelector = '#skipIntervals #quality input[value="' + optimalQuality + '"]';
+            $(qualityRadioSelector).prop("checked", true);
+            $('#skipIntervals #quality .saveValue').val(optimalQuality);
+
+            // Load Increments value
+            var incrementRadioSelector = '#skipIntervals #increments input[value="' + optimalIncrement + '"]';
+            $(incrementRadioSelector).prop("checked", true);
+            $('#skipIntervals #increments .saveValue').val(optimalIncrement);
+
+            // Load Increments Delay value
+            var sliderVal = (optimalIncrement * optimalFactor) / 1000;
+            $("#skipIntervals #increment-delay .slider").slider("option", "value", sliderVal);
+            $('#skipIntervals #increment-delay .saveValue').val(optimalFactor);
+        }
+    };
+
+    return {
+        restoreEnable: enable.restore,
+        restoreFormat: format.restore,
+        restoreQuality: quality.restore,
+        restoreIncrements: increments.restore,
+        restoreIncrementDelay: incrementDelay.restore,
+        optimalInit: optimal.init
+    }
+})(window.jQuery);
+
 function skipIntervals () {
-    $('body').on('change', '#skipIntervals #enable input[type="checkbox"]', function(event) {
-        var inputField = $(this);
-        // console.log(inputField);
-        console.log('checked state:')
-        console.log(inputField.prop("checked"));
-        var subOption = $(this).closest('.sub-option');
-        // console.log(subOption);
-
-        if (inputField.prop("checked")) {
-            subOption.find('.saveValue').val("enabled");
-
-            // Enabling settings change if module is enabled.
-            subOption.siblings('.disableSettings').removeClass('disableSettings').addClass('enableSettings');
-        } else {
-            subOption.find('.saveValue').val("disabled");
-
-            // Disabling settings change if module is disabled.
-            subOption.siblings('.enableSettings').removeClass('enableSettings').addClass('disableSettings');
-        }
-    });
-
-    // Prevent changes to settings if settings are disabled.
-    $('body').on('click', '.disableSettings', function(event) {
-        event.preventDefault();
-    });
-
-    $('body').on('change', '#skipIntervals #format input[name="format"]', function(event) {
-        var inputField = $(this);
-        var subOption = $(this).closest('.sub-option');
-        subOption.find('.saveValue').val(inputField.val());
-    });
-
-    $('body').on('change', '#skipIntervals #quality input[name="quality"]', function(event) {
-        var inputField = $(this);
-        var subOption = $(this).closest('.sub-option');
-        subOption.find('.saveValue').val(inputField.val());
-    });
-
-    $('body').on('change', '#skipIntervals #increments input[name="increments"]', function(event) {
-        var inputField = $(this);
-        var subOption = $(this).closest('.sub-option');
-        subOption.find('.saveValue').val(inputField.val());
-
-        // Emulating change event on slider by changing values. Using.trigger("change") was not working.
-        var tempSliderValue = $("#skipIntervals #increment-delay .slider").slider("option", "value");
-        $("#skipIntervals #increment-delay .slider").slider("option", "value", 0);
-        $("#skipIntervals #increment-delay .slider").slider("option", "value", tempSliderValue);
-    });
-
-    // Initiate Increment Delay Slider
-    $('#skipIntervals #increment-delay .slider').slider({
-        min: 0.5,
-        max: 10,
-        step: 0.5,
-        animate: "fast",
-        change: function(event, slider) {
-            skipIntervals_updateIncrementDelayValue(slider.value, $(this).parent());
-        }
-    });
-
-    $('#load-optimal-settings').on('click', function () {
-        var $button = $(this);
-        skipIntervals_loadOptimalSettings("tiny", 20, 75);
-
-        $button.text('Loaded!');
-        $button.addClass('notify-load');
-        setTimeout(function() {
-            $button.text('Load Optimal Settings');
-            $button.removeClass('notify-load');
-        }, 1000);
-    });
+    console.log('in skip intervals');
 }
 
 /**
@@ -77,74 +197,10 @@ function skipIntervals () {
  * @param  {[JSON]} settings [An object containing the settings recieved from chrome]
  */
 function skipIntervals_restoreSettings (settings) {
-    var enabledSettingVal; // A variable to store the boolean value of the "enabled" setting
-    if (settings.skipIntervals.enabled == "enabled") {
-        enabledSettingVal = true;
-        $('#skipIntervals .addEnableSettingState').removeClass('addEnableSettingState').addClass('enableSettings');
-    } else {
-        enabledSettingVal = false;
-        $('#skipIntervals .addEnableSettingState').removeClass('addEnableSettingState').addClass('disableSettings');
-    }
-
-    // Load Enable checkbox value
-    $('#skipIntervals #enable input[type="checkbox"]').prop("checked", enabledSettingVal);
-    $('#skipIntervals #enable .saveValue').val(settings.skipIntervals.enabled);
-
-    // Load Format radio button value
-    var formatRadioSelector = '#skipIntervals #format input[value="' + settings.skipIntervals.format + '"]';
-    $(formatRadioSelector).prop("checked", true);
-    $('#skipIntervals #format .saveValue').val(settings.skipIntervals.format);
-
-    // Load Quality radio button value
-    var qualityRadioSelector = '#skipIntervals #quality input[value="' + settings.skipIntervals.quality + '"]';
-    $(qualityRadioSelector).prop("checked", true);
-    $('#skipIntervals #quality .saveValue').val(settings.skipIntervals.quality);
-
-    // Load Increments value
-    var incrementRadioSelector = '#skipIntervals #increments input[value="' + settings.skipIntervals.increment + '"]';
-    $(incrementRadioSelector).prop("checked", true);
-    $('#skipIntervals #increments .saveValue').val(settings.skipIntervals.increment);
-
-    // Load Increments Delay value
-    var sliderVal = (settings.skipIntervals.increment * settings.skipIntervals.incrementFactor) / 1000;
-    $("#skipIntervals #increment-delay .slider").slider("option", "value", sliderVal);
-    $('#skipIntervals #increment-delay .saveValue').val(settings.skipIntervals.incrementFactor);
-}
-
-/**
- * Loads optimal setting if a user is facing trouble with choosing settings in accordance with their connection.
- * @param  {[String]}  optimalQuality
- * @param  {[Integer]} optimalIncrement
- * @param  {[Integer]} optimalFactor
- */
-function skipIntervals_loadOptimalSettings (optimalQuality, optimalIncrement, optimalFactor) {
-    // Load Quality radio button value
-    var qualityRadioSelector = '#skipIntervals #quality input[value="' + optimalQuality + '"]';
-    $(qualityRadioSelector).prop("checked", true);
-    $('#skipIntervals #quality .saveValue').val(optimalQuality);
-
-    // Load Increments value
-    var incrementRadioSelector = '#skipIntervals #increments input[value="' + optimalIncrement + '"]';
-    $(incrementRadioSelector).prop("checked", true);
-    $('#skipIntervals #increments .saveValue').val(optimalIncrement);
-
-    // Load Increments Delay value
-    var sliderVal = (optimalIncrement * optimalFactor) / 1000;
-    $("#skipIntervals #increment-delay .slider").slider("option", "value", sliderVal);
-    $('#skipIntervals #increment-delay .saveValue').val(optimalFactor);
-}
-
-/**
- * Function that is called when a change occurs in the increment delay slider
- * to update appropriate input fields.
- * @param  {[Integer]} delayVal  [Value selected by the user in the slider]
- * @param  {[DOM Element]} parentDiv [The parent div of the slider]
- */
-function skipIntervals_updateIncrementDelayValue (delayVal, parentDiv) {
-    var incrementVal = parseInt($('#skipIntervals #increments .saveValue').val());
-    var totalDelay = delayVal * 1000;
-    var incrementFactor = Math.floor(totalDelay / incrementVal);
-
-    parentDiv.find('.increment-delay-value').val(delayVal);
-    parentDiv.find('.saveValue').val(incrementFactor).trigger('change');
+    skipIntervalsModule.restoreEnable(settings);
+    skipIntervalsModule.restoreFormat(settings);
+    skipIntervalsModule.restoreQuality(settings);
+    skipIntervalsModule.restoreIncrements(settings);
+    skipIntervalsModule.restoreIncrementDelay(settings);
+    skipIntervalsModule.optimalInit();
 }
